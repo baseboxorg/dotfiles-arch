9cb3c3f00364df593db1913c55c3b398ec77a05d
Joshua Li <joshua.r.li.98@gmail.com>
2017-09-11 13:11:46 -0400

    updating link script

--- link.sh
+++ link.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-HERE="$(dirname "$(readlink -f "$0")")"
-
 [[ ! "$@" ]] && exit
+
+HERE="$(dirname "$(readlink -f "$0")")"
 
 (
 cd "$HERE"
@@ -12,7 +12,7 @@ for dotdir in "$@"; do
         # stow will refuse to link dotfiles in a directory if at least
         # one dotfile already exists, and there is no option to backup
         # clobbered dotfiles, so I made my own.
-        OUT="$(stow -v -t "$HOME" "$dotdir" 2>&1)"
+        OUT="$(stow --ignore='(server-provisioning|.gitmodules)' -v -t "$HOME" "$dotdir" 2>&1)"
         if [[ $? -eq 1 ]]; then
             BADFILES="$(echo "$OUT" | grep -oP \
                 "(?<=existing target is neither a link nor a directory: )[^ ]+")"
@@ -25,7 +25,7 @@ for dotdir in "$@"; do
             # maybe there will be race conditions with nested dotfile directories...
             # I haven't tested this yet.
             set -e
-            stow -v -t "$HOME" "$dotdir" &> /dev/null
+            stow --ignore='(server-provisioning|.gitmodules)' -v -t "$HOME" "$dotdir" &> /dev/null
             set +e
         fi
         echo "Success."
